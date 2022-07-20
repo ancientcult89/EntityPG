60ca506e320644729e982c7aa8172ec3622a1aa0
PolyakovSergey <ancientcult89@gmail.com>
2022-07-20 20:59:42 +0700

    finalized

---
+++ EntityPG/Controllers/ShoppingApiController.cs
@@ -0,0 +1,111 @@
+﻿using EntityPG.EFCore;
+using EntityPG.Model;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EntityPG.Controllers
+{    
+    [ApiController]
+    public class ShoppingApiController : ControllerBase
+    {
+        private readonly DbHelper _dbHelper;
+        public ShoppingApiController(DataContext dataContext)
+        { 
+            _dbHelper = new DbHelper(dataContext);
+        }
+
+        // GET: api/<ShoppingApiController>
+        [Route("api/[controller]/GetProducts")]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            ResponseType type = ResponseType.Success;
+            try
+            {
+                IEnumerable<ProductModel> data = _dbHelper.GetProducts();
+                if (!data.Any())
+                {
+                    type = ResponseType.NotFound;
+                }
+                return Ok(ResponseHandler.GetApiResponse(type, data));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ResponseHandler.GetExceptionResponse(ex)); ;
+            }
+        }
+
+        // GET api/<ShoppingApiController>/5
+        [HttpGet]
+        [Route("api/[controller]/GetProductsById/{id}")]
+        public IActionResult Get(int id)
+        {
+            ResponseType type = ResponseType.Success;
+            try
+            {
+                ProductModel data = _dbHelper.GetProductsById(id);
+                if (data == null)
+                {
+                    type = ResponseType.NotFound;
+                }
+                return Ok(ResponseHandler.GetApiResponse(type, data));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ResponseHandler.GetExceptionResponse(ex)); ;
+            }
+        }
+
+        // POST api/<ShoppingApiController>
+        [HttpPost]
+        [Route("api/[controller]/SaveOrder")]
+        public IActionResult Post([FromBody] OrderModel model)
+        {
+            ResponseType type = ResponseType.Success;
+            try
+            {
+                _dbHelper.SaveOrder(model);
+                return Ok(ResponseHandler.GetApiResponse(type, model));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ResponseHandler.GetExceptionResponse(ex)); ;
+            }
+        }
+
+        // PUT api/<ShoppingApiController>/5
+        [HttpPut]
+        [Route("api/[controller]/UpdateOrder/{id}")]
+        public IActionResult Put(int id, [FromBody] OrderModel model)
+        {
+            ResponseType type = ResponseType.Success;
+            try
+            {
+                _dbHelper.SaveOrder(model);
+                return Ok(ResponseHandler.GetApiResponse(type, model));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ResponseHandler.GetExceptionResponse(ex)); ;
+            }
+        }
+
+        // DELETE api/<ShoppingApiController>/5
+        [HttpDelete]
+        [Route("api/[controller]/DeleteOrder/{id}")]
+        public IActionResult Delete(int id)
+        {
+            ResponseType type = ResponseType.Success;
+            try
+            {
+                _dbHelper.DeleteOrder(id);
+                return Ok(ResponseHandler.GetApiResponse(type, "Delete"));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ResponseHandler.GetExceptionResponse(ex)); ;
+            }
+        }
+    }
+}
--- EntityPG/Model/DbHelper.cs
+++ EntityPG/Model/DbHelper.cs
@@ -9,6 +9,11 @@ namespace EntityPG.Model
         {
             _dataContext = dataContext;
         }
+
+        /// <summary>
+        /// GET
+        /// </summary>
+        /// <returns></returns>
         public List<ProductModel> GetProducts()
         { 
             List<ProductModel> responce = new List<ProductModel>();
@@ -21,6 +26,23 @@ namespace EntityPG.Model
                 size = row.size
             }));
             return responce;
+        }
+
+        /// <summary>
+        /// GET
+        /// </summary>
+        /// <returns></returns>
+        public ProductModel GetProductsById(int id)
+        {
+            //ProductModel responce = new ProductModel();
+            var row  = _dataContext.Products.Where(p => p.id.Equals(id)).FirstOrDefault();
+            return new ProductModel() { 
+                brand = row?.brand,
+                id = (int)row?.id,
+                name = row?.name,
+                price = (int)row?.price,
+                size = (int)row?.size
+            };
         }
 
         /// <summary>
@@ -47,6 +69,20 @@ namespace EntityPG.Model
                     dbTable.product = _dataContext.Products.Where(o => o.id.Equals(orderModel.product_id)).FirstOrDefault();
                     _dataContext.Orders.Add(dbTable);
                 }
+                _dataContext.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// Delete
+        /// </summary>
+        /// <param name="id"></param>
+        public void DeleteOrder(int id)
+        {
+            var order = _dataContext.Orders.Where(o => o.id.Equals(id)).FirstOrDefault();
+            if (order != null)
+            {
+                _dataContext.Orders.Remove(order);
                 _dataContext.SaveChanges();
             }
         }
